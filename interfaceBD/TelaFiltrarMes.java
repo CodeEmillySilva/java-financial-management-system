d2ff182dfba043320f358421c3cde831300429ff
CodeEmillySilva <emillystephannysilva@gmail.com>
2026-02-04 12:43:18 -0300

    Create TelaFiltrarMes.java

---
+++ interfaceBD/TelaFiltrarMes.java
@@ -0,0 +1,95 @@
+package interfaceBD;
+
+import javax.swing.JFrame;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import dadosBD.Usuario;
+
+@SuppressWarnings("serial")
+public class TelaFiltrarMes extends JFrame{
+	
+	private static final int DEFAULT_WIDTH=1000;
+	private static final int DEFAULT_HEIGHT=1000;
+
+	public TelaFiltrarMes(Usuario usuarioMenu) {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		JLabel titulo = new JLabel("ESCOLHA UM MÊS:");
+		titulo.setAlignmentX(CENTER_ALIGNMENT);
+		titulo.setFont(new Font("Arial", Font.BOLD,24));
+		
+		String [] opcoes = {"Escolha uma opção","01","02","03","04","05","06","07","08","09","10","11","12"};
+		JComboBox<String>escolha = new JComboBox<>(opcoes);
+		escolha.setAlignmentX(CENTER_ALIGNMENT);
+		escolha.setPreferredSize(new Dimension(200,20));
+		
+		JButton botao1 = new JButton("Voltar");
+		botao1.setPreferredSize(new Dimension(100,30));
+		botao1.setFont(new Font ("Arial",Font.PLAIN,18));
+		JButton botao2 = new JButton("Ver");
+		botao2.setPreferredSize(new Dimension(100,30));
+		botao2.setFont(new Font ("Arial",Font.PLAIN,18));
+		
+		JPanel botoes = new JPanel();
+		botoes.setAlignmentX(CENTER_ALIGNMENT);
+		botoes.add(botao1);
+		botoes.add(botao2);
+		
+		JPanel juncao = new JPanel();
+		juncao.setLayout(new BoxLayout(juncao,BoxLayout.Y_AXIS));
+		juncao.setBorder(new EmptyBorder(400,300,450,300));
+		juncao.add(titulo);
+		juncao.add(Box.createRigidArea(new Dimension(0,20)));
+		juncao.add(escolha);
+		juncao.add(Box.createRigidArea(new Dimension(0,20)));
+		juncao.add(botoes);
+		
+		botao1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				dispose();
+				new TelaMenu(usuarioMenu);
+
+			}
+			
+		});
+		
+		botao2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {		
+				
+				String mesEscolhido = (String) escolha.getSelectedItem();
+				
+				if(mesEscolhido.equals("Escolha uma opção")){
+					JOptionPane.showMessageDialog(null,"Por favor, escolha um mês.","Erro",JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				
+				dispose();
+				new TelaVerPorMes(mesEscolhido,usuarioMenu);
+
+			}
+			
+		});
+		
+		this.add(juncao);
+		this.setVisible(true);
+		
+	}
+}
